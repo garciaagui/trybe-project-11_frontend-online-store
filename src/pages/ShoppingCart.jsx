fa73c99f075693b21403d6166667d4ed214ae1f5
Guilherme Garcia <garciaguig@gmail.com>
2022-08-09 00:32:15 -0300

    Criação da lógica, testes ainda não estão OK

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { getCartItems } from '../services/localStorage';
+import { getCartItems,
+  addItem,
+  removeItem,
+  decreaseItem } from '../services/localStorage';
 
 class ShoppingCart extends Component {
   constructor(props) {
@@ -11,7 +14,12 @@ class ShoppingCart extends Component {
 
   componentDidMount = async () => {
     const productResult = await getCartItems();
-    console.log(productResult);
+    // console.log(productResult);
+    this.setState({ product: productResult });
+  }
+
+  componentDidUpdate = async () => {
+    const productResult = await getCartItems();
     this.setState({ product: productResult });
   }
 
@@ -29,15 +37,44 @@ class ShoppingCart extends Component {
       );
     }
     return (
-      product.map((p) => (
-        <div key={ p.id }>
-          <p data-testid="shopping-cart-product-name">{ p.title }</p>
-          <p>{ p.price }</p>
-          <p data-testid="shopping-cart-product-quantity">
-            { product.filter((pro) => pro.id === p.id).length }
-          </p>
-        </div>
-      ))
+      // Explicação de como utilizei o Reduce:
+      // 1o: Defini um array como valor inicial do acumulador (acc);
+      // 2o: Para cada elemento de product, verifiquei se havia um semelhante no array do acumulador, utilizando o atributo "id" para comparação. Caso não houvesse, o elemento em questão é acrescentado no array do acc via método push.
+      // 3o: Ao final da iteração, temos um array sem produtos repetidos no qual podemos replicar a estrutura e lógica existentes.
+      product.reduce((acc, curr) => {
+        if (!acc.some((e) => e.id === curr.id)) acc.push(curr);
+        return acc;
+      }, [])
+        .map((p) => (
+          <div key={ p.id }>
+            <p data-testid="shopping-cart-product-name">{ p.title }</p>
+            <p>{ p.price }</p>
+            <p data-testid="shopping-cart-product-quantity">
+              { product.filter((pro) => pro.id === p.id).length }
+            </p>
+            <button
+              type="button"
+              data-testid="product-increase-quantity"
+              onClick={ async () => addItem(p) }
+            >
+              +1
+            </button>
+            <button
+              type="button"
+              data-testid="product-decrease-quantity"
+              onClick={ async () => decreaseItem(p) }
+            >
+              -1
+            </button>
+            <button
+              type="button"
+              data-testid="remove-product"
+              onClick={ async () => removeItem(p) }
+            >
+              Remover do Carrinho
+            </button>
+          </div>
+        ))
     );
   }
 }
--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -35,3 +35,17 @@ export const removeItem = (item) => new Promise((resolve) => {
   saveInCart(itemsSaved.filter((i) => i.id !== item.id));
   simulateRequest(SUCCESS_STATUS)(resolve);
 });
+
+// Tive que criar uma função, pois a removeItem eliminava todos as unidades do item. Segue explicação:
+// 1o: Para conseguir a qtd do item no carrinho, utilizei a mesma lógica aplicada no ShoppingCart;
+// 2o: Vi o pessoal comentando que, caso a qtd do item seja "1", o botão não deveria diminuir a qtd (no caso, excluir o item). Criei a lógica, mas mesmo assim não passou nos testes;
+// 3o: Se a qtd for maior que 1, extraio a posição no array (LocalStorage) do primeiro elemento cujo id é igual ao do produto que desejamos eliminar. Em seguida, retiro ele do array via filter.
+export const decreaseItem = (item) => new Promise((resolve) => {
+  const itemsSaved = readItemsSaved();
+  const itemQuantity = itemsSaved.filter((i) => i.id === item.id).length;
+  if (itemQuantity > 1) {
+    const itemIndex = itemsSaved.indexOf(itemsSaved.find((i) => i.id === item.id));
+    saveInCart(itemsSaved.filter((_, index) => itemIndex !== index));
+  }
+  simulateRequest(SUCCESS_STATUS)(resolve);
+});
